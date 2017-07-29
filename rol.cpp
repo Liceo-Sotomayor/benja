269af9920e3f704d311b200bc4affd6c740da8e4
benja <benjabustoss2003@gmail.com>
2017-07-29 10:05:12 -0400

    descontando vida

--- rol.cpp
+++ rol.cpp
@@ -6,19 +6,26 @@ using namespace std;
 int main() {
     srand (time(NULL));
     int arma, escudo, danio;
-    arma = rand()%6+1;
-    arma += rand()%6+1;
-    arma += 3;
+    int vida = 100;
+    
+    while (vida > 0) {
+        arma = rand()%6+1;
+        arma += rand()%6+1;
+        arma += 3;
 
-    escudo = rand()%4+1;
-    escudo += rand()%4+1;
-    escudo += 1;
+        escudo = rand()%4+1;
+        escudo += rand()%4+1;
+        escudo += 1;
 
-    danio = arma - escudo;
-  
-    if (danio > 0) {
-        cout << " el daño es " << danio << endl;
-    } else {
-        cout << " no sufrio daño "  << endl;
+        danio = arma - escudo;
+    
+        if (danio > 0) {
+            vida -= danio;
+            cout << " el daño es " << danio << " vida restante " << vida << endl;
+        } else {
+            cout << " no sufrio daño "  << endl;
+        }
     }
+    cout << " estas muerto " << endl;
+    
 }
